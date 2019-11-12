13c5d85ba8792238bab975ec7e3e732f4081a3e3
David Mendez <mendezdavid68@gmail.com>
2019-11-12 09:15:08 -0700

    Post and notes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,51 @@
+
+
+class PagesController < ApplicationController
+
+  def index
+    @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end  
+
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(pages_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def create 
+    @page = Page.new(pages_params)
+
+    if @page.save
+      redirect_to pages_path
+    else
+      render :new
+  end
+end
+
+private
+
+   def pages_params
+     params.require(:page).permit(:title, :author, :body)
+   end
+
+end
